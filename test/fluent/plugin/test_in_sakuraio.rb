cbc7e98d9f6578bc2066ac96af5f50c3b22dd0ea
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2017-11-05 00:08:02 +0900

    Use Fluent::TimeParser in test

--- test/fluent/plugin/test_in_sakuraio.rb
+++ test/fluent/plugin/test_in_sakuraio.rb
@@ -5,6 +5,7 @@ require 'fluent/test/driver/input'
 class SakuraIOInputTest < Test::Unit::TestCase
   def setup
     Fluent::Test.setup
+    @time_parser = Fluent::TimeParser.new(nil)
   end
 
   CONFIG = %([
@@ -22,31 +23,31 @@ class SakuraIOInputTest < Test::Unit::TestCase
     end
   end
 
-  expected_records = [
-    [
-      'uXXXXXXXXXXX.connection',
-      Time.parse('2017-04-06T07:39:29.703232943Z').to_i,
-      { 'module' => 'uXXXXXXXXXXX', 'is_online' => true }
-    ],
-    [
-      'uXXXXXXXXXXX.location',
-      Time.parse('2017-04-04T01:31:19.6431197Z').to_i,
-      { 'module' => 'uXXXXXXXXXXX', 'latitude' => 34.704254, 'longitude' => 135.494691, 'range_m' => 0 }
-    ],
-    [
-      'uXXXXXXXXXXX.channels.0',
-      Time.parse('2017-04-06T07:39:29.703232943Z').to_i,
-      { 'module' => 'uXXXXXXXXXXX', 'channel' => 0, 'type' => 'I', 'value' => 10 }
-    ],
-    [
-      'uXXXXXXXXXXX.channels.1',
-      Time.parse('2017-04-06T07:39:30.703232943Z').to_i,
-      { 'module' => 'uXXXXXXXXXXX', 'channel' => 1, 'type' => 'L', 'value' => 100 }
-    ]
-  ]
-
   sub_test_case 'emit' do
-    test 'test expects plugin emits events 2 times' do
+    test 'test expects plugin emits events 4 times' do
+      expected_records = [
+        [
+          'uXXXXXXXXXXX.connection',
+          @time_parser.parse('2017-04-06T07:39:29.703232943Z'),
+          { 'module' => 'uXXXXXXXXXXX', 'is_online' => true }
+        ],
+        [
+          'uXXXXXXXXXXX.location',
+          @time_parser.parse('2017-04-04T01:31:19.6431197Z'),
+          { 'module' => 'uXXXXXXXXXXX', 'latitude' => 34.704254, 'longitude' => 135.494691, 'range_m' => 0 }
+        ],
+        [
+          'uXXXXXXXXXXX.channels.0',
+          @time_parser.parse('2017-04-06T07:39:29.703232943Z'),
+          { 'module' => 'uXXXXXXXXXXX', 'channel' => 0, 'type' => 'I', 'value' => 10 }
+        ],
+        [
+          'uXXXXXXXXXXX.channels.1',
+          @time_parser.parse('2017-04-06T07:39:30.703232943Z'),
+          { 'module' => 'uXXXXXXXXXXX', 'channel' => 1, 'type' => 'L', 'value' => 100 }
+        ]
+      ]
+
       test_server = TestServer.new
       t = Thread.new do
         test_server.listen('127.0.0.1', 8080)
